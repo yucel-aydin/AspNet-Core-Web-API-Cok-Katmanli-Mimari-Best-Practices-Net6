45f7f29e27204c3ae8ec7a120db6fa41f026b88e
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-21 15:54:49 +0300

    Core katmanına unitofwork interface i eklendi.

---
+++ NLayer/NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.UnitOfWorks
+{
+    /// <summary>
+    /// Veritabanına yapılacak işlemleri toplu bir şekilde tek bir transaction üzerinden yönetilmesine izin verir.
+    /// Farklı repository de yapılan değişikler ayrı ayrı savechange çağrılarak db yazılır. 
+    /// Unitofwork sayesinde farklı repodaki işlemler tek seferde db ye yazılır. Eğer birinde sorun olursa bütün işlemler geri alınır(Rolback) otomatik olarak.
+    /// Savechanges kontrolünü sağlar.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Comit();
+    }
+}
