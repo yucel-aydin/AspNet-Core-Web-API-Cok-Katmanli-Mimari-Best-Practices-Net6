47b6552485e95306e35c54c53b8d53edea328bbf
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-21 17:14:24 +0300

    Repository katmanına Entity configuration sınıfları eklendi.

--- NLayer/NLayer.Repository/AppDbContext.cs
+++ NLayer/NLayer.Repository/AppDbContext.cs
@@ -3,6 +3,7 @@ using NLayer.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +17,12 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        // model oluşurken bu metot çalışır.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // Bulunduğu assemplydeki tüm configurationları bulur ve uygular.
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
