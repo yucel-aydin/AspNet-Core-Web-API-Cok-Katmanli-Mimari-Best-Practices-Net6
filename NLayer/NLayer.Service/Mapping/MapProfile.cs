3b96dea89fb06601d6a9418db005d9e614a54307
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-22 23:16:00 +0300

    Automapper kütüphanesi eklendi

---
+++ NLayer/NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            // ReverseMap ile iki taraflı map yapılabilir yazılmazsa tek taraflı map işlemi yapılır.
+
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
