8e9fc798ce75f0c2efabe94084e8473419629b36
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-25 13:46:17 +0300

    Global Exception Handler eklendi

---
+++ NLayer/NLayer.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.DTOs;
+using NLayer.Service.Exceptions;
+using System.Text.Json;
+
+namespace NLayer.API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                // Run ile sonlandırıcı bir middleware oluşturuyoruz. Yani uygulamada bir hata oluştuğunda bu middleware çalışacak ve sonlanacak.
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        _ => 500
+                    };
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentResponseDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+               
+                });
+            });
+        }
+    }
+}
