bc45bd047af19c956a7fdc5db7833fd5af575923
YUCELAYDIN\yucel <yucel.aydin@arimars.com>
2022-10-25 13:29:44 +0300

    fluentvalidation eklendi. Filter attribute eklendi

--- NLayer/NLayer.API/Program.cs
+++ NLayer/NLayer.API/Program.cs
@@ -1,4 +1,8 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NLayer.API.Filters;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -7,14 +11,22 @@ using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
 using NLayer.Service.Mapping;
 using NLayer.Service.Services;
+using NLayer.Service.Validations;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Fluentvalidation için filter attribute ekliyoruz.
+builder.Services
+    .AddControllers(options =>options.Filters.Add(new ValidateFilterAttribute()))
+    .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
 
-builder.Services.AddControllers();
-
+// Kendi filtremizi yazdýðýmýz için default api filter ý pasif hale getiriyoruz.
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
